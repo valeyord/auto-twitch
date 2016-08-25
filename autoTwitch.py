7f53eaba48e9d3a12748c18a66f0c4a4a3c276bb
Rodrigo <rohiro@outlook.com>
2016-08-25 10:55:47 -0300

    create chrome-data-directories for "chrome new session" bug workaround

--- autoTwitch.py
+++ autoTwitch.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from subprocess import Popen
-Popen(["google-chrome --new-window --window-position=0,0 --window-size=1000,766 \
- --app='https://www.twitch.tv'"], shell=True)
-Popen(["google-chrome --new-window --window-position=1000,1000 --window-size=50,50\
---app='https://www.google.com'"],shell=True)
+Popen(["google-chrome \
+--user-data-dir='/home/rodrigo/python/myScripts/auto-twitch/chrome-video-data' \
+--new-window --window-position=0,0 --window-size=1000,766 \
+--app='https://www.google.com'"], shell=True)
+Popen(["google-chrome \
+--user-data-dir='/home/rodrigo/python/myScripts/auto-twitch/chrome-chat-data' \
+--new-window --window-position=1000,1000 --window-size=250,250 \
+--app='https://www.twitch.tv'"],shell=True)
