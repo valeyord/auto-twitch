f01f40a2efa8663bf85206697fe1c9a61f9ce7ae
Rodrigo <rohiro@outlook.com>
2016-08-25 11:17:03 -0300

    add amaz video and chat to links

--- autoTwitch.py
+++ autoTwitch.py
@@ -3,8 +3,8 @@ from subprocess import Popen
 Popen(["google-chrome \
 --user-data-dir='/home/rodrigo/python/myScripts/auto-twitch/chrome-video-data' \
 --new-window --window-position=0,0 --window-size=990,768 \
---app='https://www.google.com'"], shell=True)
+--app='https://player.twitch.tv/?volume=0.5&channel=amazhs'"], shell=True)
 Popen(["google-chrome \
 --user-data-dir='/home/rodrigo/python/myScripts/auto-twitch/chrome-chat-data' \
 --new-window --window-position=1000,0 --window-size=366,768 \
---app='https://www.twitch.tv'"],shell=True)
+--app='https://www.twitch.tv/amazhs/chat?popout='"],shell=True)
