9e5bba312918e001e557104ecf1f9fd432bec79f
Rodrigo <rohiro@outlook.com>
2016-08-24 21:44:08 -0300

    add optimized video window size (1000x766)

--- autoTwitch.py
+++ autoTwitch.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from subprocess import call
-call(["google-chrome --window-size=50,50 --window-position=0,0] \
+call(["google-chrome --window-size=1000,766 --window-position=0,0] \
 --app='https://www.twitch.tv'"], shell=True)
