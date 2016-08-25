91414c01fef0e32c3bcd0370c1a29d2585f6a8cb
Rodrigo <rohiro@outlook.com>
2016-08-24 22:14:41 -0300

    add multiple chrome launch, but with the same flag

--- autoTwitch.py
+++ autoTwitch.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from subprocess import call
-call(["google-chrome --window-size=1000,766 --window-position=0,0] \
---app='https://www.twitch.tv'"], shell=True)
+call(["google-chrome --window-position=0,0 --window-size=1000,766 \
+ --app='https://www.twitch.tv' & google-chrome --new-window --window-position=1000,1000\
+ --app='https://www.google.com'"], shell=True)
