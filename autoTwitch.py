9a94e070aa4f4b9fc7a9334fdf2c9847950ae0fd
Rodrigo <rohiro@outlook.com>
2016-08-25 10:25:37 -0300

    First Popen test

--- autoTwitch.py
+++ autoTwitch.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from subprocess import call
-call(["google-chrome --window-position=0,0 --window-size=1000,766 \
- --app='https://www.twitch.tv' & google-chrome --new-window --window-position=1000,1000\
- --app='https://www.google.com'"], shell=True)
+from subprocess import Popen
+Popen(["google-chrome --new-window --window-position=0,0 --window-size=1000,766 \
+ --app='https://www.twitch.tv'"], shell=True)
+Popen(["google-chrome --new-window --window-position=1000,1000 --window-size=50,50\
+--app='https://www.google.com'"],shell=True)
