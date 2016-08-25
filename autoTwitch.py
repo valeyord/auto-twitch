31d388b452b836a93a2bfbf51a6271fc95b6956b
Rodrigo <rohiro@outlook.com>
2016-08-25 10:59:57 -0300

    add perfect window size

--- autoTwitch.py
+++ autoTwitch.py
@@ -2,9 +2,9 @@
 from subprocess import Popen
 Popen(["google-chrome \
 --user-data-dir='/home/rodrigo/python/myScripts/auto-twitch/chrome-video-data' \
---new-window --window-position=0,0 --window-size=1000,766 \
+--new-window --window-position=0,0 --window-size=990,768 \
 --app='https://www.google.com'"], shell=True)
 Popen(["google-chrome \
 --user-data-dir='/home/rodrigo/python/myScripts/auto-twitch/chrome-chat-data' \
---new-window --window-position=1000,1000 --window-size=250,250 \
+--new-window --window-position=1000,0 --window-size=366,768 \
 --app='https://www.twitch.tv'"],shell=True)
