c2a5638c537fe20a19163db8e9c4125d7b3503de
Rodrigo <rohiro@outlook.com>
2016-08-26 22:04:25 -0300

    add comments

--- autoTwitch.py
+++ autoTwitch.py
@@ -1,15 +1,20 @@
-#Author: Rodrigo 'valeyord' Lima
-#This script opens a twitch channel in popout video and popout chat window-size
-#Version 0.2
+# Author: Rodrigo 'valeyord' Lima
+# This script opens a twitch channel in popout video and popout chat window-size
+# Version 0.2
 # -*- coding: utf-8 -*-
 from subprocess import Popen, call
+# Clear terminal
 call(["clear"], shell=True)
+# Save path location to store chrome-data directories (one-time/prototype)
+PATH = "/home/rodrigo/python/myScripts/auto-twitch/"
+# Print saved channels (prototype)
 print("""My Saved Channels:
     -amazhs
     -trumpsc
     """)
-PATH = "/home/rodrigo/python/myScripts/auto-twitch/"
+# Read the channel to open
 channel = raw_input("\nType a Twitch Channel: ")
+# Call google-chrome windows from terminal
 Popen(["google-chrome \
 --user-data-dir='{}chrome-video-data' \
 --new-window --window-position=0,0 --window-size=990,768 \
