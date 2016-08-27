2bed0c6c75934f06423447b26c43a3ffd90a07d9
Rodrigo <rohiro@outlook.com>
2016-08-26 22:50:23 -0300

    add option to save channels in a txt file

--- autoTwitch.py
+++ autoTwitch.py
@@ -3,23 +3,31 @@
 # Version 0.2
 # -*- coding: utf-8 -*-
 from subprocess import Popen, call
-# Clear terminal
-call(["clear"], shell=True)
-# Save path location to store chrome-data directories (one-time/prototype)
-PATH = "/home/rodrigo/python/myScripts/auto-twitch/"
-# Print saved channels (prototype)
-print("""My Saved Channels:
-    -amazhs
-    -trumpsc
-    """)
-# Read the channel to open
-channel = raw_input("\nType a Twitch Channel: ")
+PATH ="/home/rodrigo/python/myScripts/auto-twitch/"
+def mainMenu():
+    call(["clear"], shell=True)
+    with open("/home/rodrigo/python/myScripts/auto-twitch/savedChannels.txt", "r+") as savedChannels:
+        print(savedChannels.read())
+    print("Type 'add channel' to add a new channel to the list")
+    channel = raw_input("\nType a Twitch Channel: ")
+    if channel[:4] == "add ":
+        AddChannel(channel[4:])
+    else:
+        OpenTwitch(channel)
+
+def AddChannel(x):
+    with open("/home/rodrigo/python/myScripts/auto-twitch/savedChannels.txt", "a") as savedChannels:
+        savedChannels.write(x + "\n")
+    mainMenu()
 # Call google-chrome windows from terminal
-Popen(["google-chrome \
---user-data-dir='{}chrome-video-data' \
---new-window --window-position=0,0 --window-size=990,768 \
---app='https://player.twitch.tv/?volume=0.5&channel={}'".format(PATH, channel)], shell=True)
-Popen(["google-chrome \
---user-data-dir='{}chrome-chat-data' \
---new-window --window-position=1000,0 --window-size=366,768 \
---app='https://www.twitch.tv/{}/chat?popout='".format(PATH, channel)],shell=True)
+def OpenTwitch(y):
+    Popen(["google-chrome \
+    --user-data-dir='{}chrome-video-data' \
+    --new-window --window-position=0,0 --window-size=990,768 \
+    --app='https://player.twitch.tv/?volume=0.5&channel={}'".format(PATH, y)], shell=True)
+    Popen(["google-chrome \
+    --user-data-dir='{}chrome-chat-data' \
+    --new-window --window-position=1000,0 --window-size=366,768 \
+    --app='https://www.twitch.tv/{}/chat?popout='".format(PATH, y)],shell=True)
+
+mainMenu()
